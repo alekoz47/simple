00fbbbb97293f3f764e509c9761f5144d22c34c5
Alexander Koziol <alekoz47@gmail.com>
2016-11-15 18:14:36 -0500

    update code design

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,64 +1,49 @@
+
+// basic analog pebble watchface
+
+//================================
+//Require:
+
 var rocky = require('rocky');
 
+//================================
+//Functions:
+
 function fractionToRadian(fraction) {
-  return fraction * 2 * Math.PI;
+	return fraction * 2 * Math.PI;
 }
 
 function drawHand(ctx, cx, cy, angle, length, color) {
-  // Find the end points
-  var x2 = cx + Math.sin(angle) * length;
-  var y2 = cy - Math.cos(angle) * length;
-
-  // Configure how we want to draw the hand
-  ctx.lineWidth = 8;
-  ctx.strokeStyle = color;
-
-  // Begin drawing
-  ctx.beginPath();
-
-  // Move to the center point, then draw the line
-  ctx.moveTo(cx, cy);
-  ctx.lineTo(x2, y2);
-
-  // Stroke the line (output to display)
-  ctx.stroke();
+	var x2 = cx + Math.sin(angle) * length;
+	var y2 = cy - Math.cos(angle) * length;
+	ctx.lineWidth = 8;
+	ctx.strokeStyle = color;
+	ctx.beginPath();
+	ctx.moveTo(cx, cy);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
 }
 
+//================================
+//Run:
+
 rocky.on('draw', function(event) {
-  var ctx = event.context;
-  var d = new Date();
-
-  // Clear the screen
-  ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-
-  // Determine the width and height of the display
-  var w = ctx.canvas.unobstructedWidth;
-  var h = ctx.canvas.unobstructedHeight;
-
-  // Determine the center point of the display
-  // and the max size of watch hands
-  var cx = w / 2;
-  var cy = h / 2;
-
-  // -20 so we're inset 10px on each side
-  var maxLength = (Math.min(w, h) - 20) / 2;
-
-  // Calculate the minute hand angle
-  var minuteFraction = (d.getMinutes()) / 60;
-  var minuteAngle = fractionToRadian(minuteFraction);
-
-  // Draw the minute hand
-  drawHand(ctx, cx, cy, minuteAngle, maxLength, "white");
-
-  // Calculate the hour hand angle
-  var hourFraction = (d.getHours() % 12 + minuteFraction) / 12;
-  var hourAngle = fractionToRadian(hourFraction);
-
-  // Draw the hour hand
-  drawHand(ctx, cx, cy, hourAngle, maxLength * 0.6, "lightblue");
+	var ctx = event.context;
+	var d = new Date();
+	var w = ctx.canvas.unobstructedWidth;
+	var h = ctx.canvas.unobstructedHeight;
+	var cx = w / 2;
+	var cy = h / 2;
+	var maxLength = (Math.min(w, h) - 20) / 2;
+	var minuteFraction = (d.getMinutes()) / 60;
+	var minuteAngle = fractionToRadian(minuteFraction);
+	var hourFraction = (d.getHours() % 12 + minuteFraction) / 12;
+	var hourAngle = fractionToRadian(hourFraction);
+	ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+	drawHand(ctx, cx, cy, minuteAngle, maxLength, "white");
+	drawHand(ctx, cx, cy, hourAngle, maxLength * 0.6, "lightblue");
 });
 
 rocky.on('minutechange', function(event) {
-  // Request the screen to be redrawn on next pass
-  rocky.requestDraw();
+	rocky.requestDraw();
 });
