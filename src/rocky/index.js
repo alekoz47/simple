0bd1f3bfafc492eed0c2d21085e89970a722f01d
Alexander Koziol <alekoz47@gmail.com>
2017-01-09 10:29:34 -0500

    add settings

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,11 +1,38 @@
-//================================
-//Variables:
-
 var rocky = require("rocky");
-var handColor = "red";
+var settings = null;
+var handColor = "white";
 
-//================================
-//Functions:
+function drawFace(ctx) {
+	var d = new Date();
+	var w = ctx.canvas.unobstructedWidth;
+	var h = ctx.canvas.unobstructedHeight;
+	var cx = w / 2;
+	var cy = h / 2;
+	var maxLength = (Math.min(w, h) - 10) / 2;
+	var minuteFraction = d.getMinutes() / 60;
+	var minuteAngle = fractionToRadian(minuteFraction);
+	var hourFraction = (d.getHours() % 12 + minuteFraction) / 12;
+	var hourAngle = fractionToRadian(hourFraction);
+	
+	ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+	
+	for (var ii = 0; ii < 360; ii += 6) {
+		var angle = fractionToRadian(ii / 360);
+		var x = cx + Math.sin(angle) * maxLength;
+		var y = cy - Math.cos(angle) * maxLength;
+		if (ii % 30 === 0) {
+			drawTick(ctx, x, y, angle, maxLength * 0.1, "lightgrey");
+		} else {
+			drawTick(ctx, x, y, angle, maxLength * 0.05, "darkgrey");
+		}
+	}
+	drawText(ctx, w - 45, cy - ctx.measureText("0").height, d.getDate().toString());
+	ctx.strokeStyle = handColor;
+	ctx.fillStyle = handColor;
+	drawHand(ctx, cx, cy, minuteAngle, maxLength * 0.8, 7);
+	drawHand(ctx, cx, cy, hourAngle, maxLength * 0.5, 10);
+	drawCenter(ctx, cx, cy);
+}
 
 function fractionToRadian(fraction) {
 	return fraction * 2 * Math.PI;
@@ -23,7 +50,6 @@ function drawHand(ctx, cx, cy, angle, length, width) {
 	var y = cy - Math.cos(angle) * length;
 	
 	ctx.lineWidth = width;
-	ctx.strokeStyle = handColor;
 	drawLine(ctx, cx, cy, x, y);
 	
 	if (width === 10) {
@@ -50,44 +76,30 @@ function drawText(ctx, x, y, text) {
 }
 
 function drawCenter(ctx, cx, cy) {
-	ctx.fillStyle = handColor;
 	ctx.rockyFillRadial(cx, cy, 0, 4, 0, 2 * Math.PI);
 }
 
-//================================
-//Run:
-
 rocky.on("draw", function(event) {
-	var ctx = event.context;
-	var d = new Date();
-	var w = ctx.canvas.unobstructedWidth;
-	var h = ctx.canvas.unobstructedHeight;
-	var cx = w / 2;
-	var cy = h / 2;
-	var maxLength = (Math.min(w, h) - 10) / 2;
-	var minuteFraction = (d.getMinutes()) / 60;
-	var minuteAngle = fractionToRadian(minuteFraction);
-	var hourFraction = (d.getHours() % 12 + minuteFraction) / 12;
-	var hourAngle = fractionToRadian(hourFraction);
-	
-	ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-	
-	for (var ii = 0; ii < 360; ii += 6) {
-		var angle = fractionToRadian(ii / 360);
-		var x = cx + Math.sin(angle) * maxLength;
-		var y = cy - Math.cos(angle) * maxLength;
-		if (ii % 30 === 0) {
-			drawTick(ctx, x, y, angle, maxLength * 0.1, "lightgrey");
-		} else {
-			drawTick(ctx, x, y, angle, maxLength * 0.05, "darkgrey");
-		}
-	}
-	drawText(ctx, w - 45, cy - ctx.measureText("0").height, d.getDate().toString());
-	drawHand(ctx, cx, cy, minuteAngle, maxLength * 0.8, 7);
-	drawHand(ctx, cx, cy, hourAngle, maxLength * 0.5, 10);
-	drawCenter(ctx, cx, cy);
+	drawFace(event.context);
 });
 
 rocky.on("minutechange", function(event) {
 	rocky.requestDraw();
 });
+
+rocky.on("message", function(event) {
+  	settings = event.data;
+	switch(settings.handColor) {
+		case "blue":
+			handColor = "blue";
+			break;
+		case "red":
+			handColor = "red";
+			break;
+		default:
+			handColor = "white";
+	}
+  	rocky.requestDraw();
+});
+
+rocky.postMessage({command: "settings"});
